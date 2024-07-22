ff8e792eec3b0e68817ff2abeb29b43e4c865f5d
GoddessPersephone <leal3work@gmail.com>
2024-07-22 13:35:56 -0300

    Reformulação

---
+++ frontend/src/app/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { routes } from '../app.routes';
+
+@Component({
+  selector: 'app-home',
+  standalone:  true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
